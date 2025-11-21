8071672b5041a6343c586b0b6150857eca645232
melosabsly-bit <melosabsly@gmail.com>
2025-11-21 03:12:09 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,render_template
+import joblib
 
 app = Flask(__name__)
 
@@ -9,8 +10,20 @@ def index():
 @app.route("/main",methods=["GET","POST"])
 def main():
     q = request.form.get("q")
-    print(q)
+    print("q")
     return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
+@app.route("/dbs_prediction",methods=["GET","POST"])
+def dbs_prediction():
+    q = float(request.form.get("q"))
+    print("q")
+    model = joblib.load("dbs.pkl")
+    r = model.predict([[q]])
+    return(render_template("dbs_prediction.html", r=r[0][0]))
 
 if __name__ == "__main__":
     app.run()
